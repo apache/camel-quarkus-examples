d83017fa70777f901071459ac02fb921019d0e89
Maria Arias de Reyna <ariasdereyna@redhat.com>
2021-09-21 14:49:15 +0200

    example(jdbc-datastore): #3056 Adding new simple example

---
+++ jdbc-datasource/src/main/java/org/apache/camel/quarkus/component/jdbc/model/Camel.java
@@ -0,0 +1,35 @@
+package org.apache.camel.quarkus.component.jdbc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Camel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String timestamp;
+
+    public Camel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
