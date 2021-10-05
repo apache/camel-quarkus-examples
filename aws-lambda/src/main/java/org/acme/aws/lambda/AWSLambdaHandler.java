612b3d8c4525fec9ef6ad82bf4bda8c64d2c6bc0
Peter Palaga <ppalaga@redhat.com>
2021-10-05 21:32:32 +0200

    Unify package names for the for the test in camel-quarkus-examples https://github.com/apache/camel-quarkus/issues/3152

---
+++ aws-lambda/src/main/java/org/acme/aws/lambda/AWSLambdaHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.aws.lambda;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.camel.ProducerTemplate;
+
+@Named("awsLambdaHandler")
+public class AWSLambdaHandler implements RequestHandler<Person, String> {
+
+    @Inject
+    ProducerTemplate template;
+
+    @Override
+    public String handleRequest(Person input, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("Calling Camel Route :)");
+        return template.requestBody("direct:input", input, String.class);
+    }
+}
---
+++ file-bindy-ftp/src/main/java/org/acme/bindy/ftp/BookGenerator.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.bindy.ftp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+/**
+ * Processor to generate {@Book}s with a random set of data.
+ */
+@ApplicationScoped
+@Named
+public class BookGenerator implements Processor {
+
+    private static final String[] BOOK_GENRES = { "Action", "Crime", "Horror" };
+
+    private static final String[] BOOK_DESCRIPTION = {
+            "Awesome",
+            "Amazing",
+            "Fantastic",
+            "Incredible",
+            "Tremendous",
+    };
+
+    private static final String[] FIRST_NAMES = {
+            "Fyodor",
+            "Jane",
+            "Leo",
+            "Oscar",
+            "William",
+    };
+
+    private static final String[] LAST_NAMES = {
+            "Austen",
+            "Dostoevsky",
+            "Shakespeare",
+            "Tolstoy",
+            "Wilde",
+    };
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Random random = new Random();
+        List<Book> books = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            String genre = BOOK_GENRES[random.nextInt(BOOK_GENRES.length)];
+            String description = BOOK_DESCRIPTION[random.nextInt(BOOK_DESCRIPTION.length)];
+            String title = String.format("The %s book of %s #%d", description, genre, i);
+
+            String firstName = FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
+            String lastName = LAST_NAMES[random.nextInt(LAST_NAMES.length)];
+            String author = String.format("%s %s", firstName, lastName);
+
+            Book book = new Book();
+            book.setId(i);
+            book.setAuthor(author);
+            book.setTitle(title);
+            book.setGenre(genre);
+            books.add(book);
+        }
+
+        exchange.getMessage().setBody(books);
+    }
+}
