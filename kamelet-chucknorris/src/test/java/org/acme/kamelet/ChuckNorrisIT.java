5e8a5090a8b829181124b138263b1c490d03a780
James Netherton <jamesnetherton@gmail.com>
2022-09-21 11:44:14 +0100

    Disable kamelet-chucknorris tests due to apache/camel-quarkus#4111

--- kamelet-chucknorris/src/test/java/org/acme/kamelet/ChuckNorrisIT.java
+++ kamelet-chucknorris/src/test/java/org/acme/kamelet/ChuckNorrisIT.java
@@ -17,7 +17,9 @@
 package org.acme.kamelet;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("https://github.com/apache/camel-quarkus/issues/4111")
 @QuarkusIntegrationTest
 class ChuckNorrisIT extends ChuckNorrisTest {
 
