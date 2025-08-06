f3696eaeeab77e74caa7b0b82099c401df83f80b
JiriOndrusek <ondrusek.jiri@gmail.com>
2025-08-06 16:18:28 +0200

    Add Aws2-s3 example

---
+++ aws2-s3/src/test/java/org/acme/aws2/s3/Aws2S3Test.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.acme.aws2.s3;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import io.quarkus.test.common.WithTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.awaitility.Awaitility.await;
+
+@WithTestResource(Aws2S3TestResource.class)
+@QuarkusTest
+class Aws2S3Test {
+
+    @Test
+    public void testSendAndReceive() {
+        await().atMost(10L, TimeUnit.SECONDS).pollDelay(1, TimeUnit.SECONDS)
+                .untilAsserted(() -> Assertions
+                        .assertTrue(new String(Files.readAllBytes(Paths.get("target/quarkus.log")), StandardCharsets.UTF_8)
+                                .contains("Received body: Hello from Camel via aws2-s3!")));
+    }
+}
