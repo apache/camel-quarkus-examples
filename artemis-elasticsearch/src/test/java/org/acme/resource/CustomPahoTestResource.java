ca69dc29c6ec7c48cc118022f7d07ff90f282766
root <root@beaver-13.bkr.lab.eng.rdu2.dc.redhat.com>
2025-11-14 05:11:23 -0500

    Updated code as per comments

--- artemis-elasticsearch/src/test/java/org/acme/resource/CustomPahoTestResource.java
+++ artemis-elasticsearch/src/test/java/org/acme/resource/CustomPahoTestResource.java
@@ -25,7 +25,6 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class CustomPahoTestResource implements QuarkusTestResourceLifecycleManager {
-    private static String IMAGE_NAME;
     private static final String AMQ_USER = "admin";
     private static final String AMQ_PASSWORD = "admin";
     private static final String AMQ_EXTRA_ARGS = "--relax-jolokia --no-autotune --mapped --no-fsync --java-options=-Dbrokerconfig.maxDiskUsage=-1";
@@ -34,13 +33,11 @@ public class CustomPahoTestResource implements QuarkusTestResourceLifecycleManag
 
     @Override
     public Map<String, String> start() {
-    if ("ppc64le".equals(SystemUtils.OS_ARCH))
-            IMAGE_NAME = "icr.io/ppc64le-oss/activemq-artemis-broker-ppc64le:2.0.2";
-    else
-	    IMAGE_NAME = "quay.io/artemiscloud/activemq-artemis-broker:1.0.26";
+        String imageName = "quay.io/artemiscloud/activemq-artemis-broker:1.0.26";
+        if ("ppc64le".equals(SystemUtils.OS_ARCH))
+            imageName = "icr.io/ppc64le-oss/activemq-artemis-broker-ppc64le:2.0.2";
 
-
-        container = new GenericContainer<>(DockerImageName.parse(IMAGE_NAME))
+        container = new GenericContainer<>(DockerImageName.parse(imageName))
                 .withExposedPorts(61616, 8161, 1883)
                 .withEnv("AMQ_USER", AMQ_USER)
                 .withEnv("AMQ_PASSWORD", AMQ_PASSWORD)
--- artemis-elasticsearch/src/test/java/org/acme/resource/ElasticSearchTestResource.java
+++ artemis-elasticsearch/src/test/java/org/acme/resource/ElasticSearchTestResource.java
@@ -29,17 +29,18 @@ import org.testcontainers.utility.DockerImageName;
 
 public class ElasticSearchTestResource implements QuarkusTestResourceLifecycleManager {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticSearchTestResource.class);
-
-    private static String IMAGE_NAME = "mirror.gcr.io/elastic/elasticsearch:8.13.2";
+    private static String IMAGE_NAME;
     private ElasticsearchContainer container;
 
 
     @Override
     public Map<String, String> start() {
 
-	if ("ppc64le".equals(SystemUtils.OS_ARCH)) {
+       if ("ppc64le".equals(SystemUtils.OS_ARCH)) {
             IMAGE_NAME = "icr.io/ppc64le-oss/elasticsearch-ppc64le:8.3.3";
-        }
+       }
+       else
+            IMAGE_NAME = "mirror.gcr.io/elastic/elasticsearch:8.13.2";
 
         DockerImageName imageName = DockerImageName.parse(IMAGE_NAME).asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch");
         container = new ElasticsearchContainer(imageName)
