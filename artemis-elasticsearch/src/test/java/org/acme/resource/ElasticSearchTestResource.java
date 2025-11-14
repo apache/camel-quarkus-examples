47702fcdb2e284b6d9b20d07fdfd13ec17cf0ca8
Prasanna Archananil <prasanna.marathe@ibm.com>
2025-11-14 17:34:37 +0530

    Enable IBM Power support for artemis-elasticsearch example

--- artemis-elasticsearch/src/test/java/org/acme/resource/ElasticSearchTestResource.java
+++ artemis-elasticsearch/src/test/java/org/acme/resource/ElasticSearchTestResource.java
@@ -20,6 +20,7 @@ package org.acme.resource;
 import java.util.Map;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -28,16 +29,20 @@ import org.testcontainers.utility.DockerImageName;
 
 public class ElasticSearchTestResource implements QuarkusTestResourceLifecycleManager {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticSearchTestResource.class);
-
-    private static final String IMAGE_NAME = "mirror.gcr.io/elastic/elasticsearch:8.13.2";
+    private static String IMAGE_NAME;
     private ElasticsearchContainer container;
 
     @Override
     public Map<String, String> start() {
 
-        DockerImageName imageName = DockerImageName.parse(IMAGE_NAME);
-        container = new ElasticsearchContainer(
-                imageName.asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch"))
+        if ("ppc64le".equals(SystemUtils.OS_ARCH)) {
+            IMAGE_NAME = "icr.io/ppc64le-oss/elasticsearch-ppc64le:8.3.3";
+        } else
+            IMAGE_NAME = "mirror.gcr.io/elastic/elasticsearch:8.13.2";
+
+        DockerImageName imageName = DockerImageName.parse(IMAGE_NAME)
+                .asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch");
+        container = new ElasticsearchContainer(imageName)
                 .withExposedPorts(9200)
                 .withEnv("discovery.type", "single-node")
                 .withEnv("xpack.security.enabled", "false")
