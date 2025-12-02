44d4cca3122277827ba8267dea527295b9ec22bc
Lukas Lowinger <llowinge@redhat.com>
2025-12-02 13:03:25 +0100

    [resolves #7918] Demonstrate using CXF client producer

--- cxf-soap/src/main/java/org/acme/cxf/soap/utils/CxfServerUtils.java
+++ cxf-soap/src/main/java/org/acme/cxf/soap/utils/CxfServerUtils.java
@@ -14,17 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.acme.cxf.soap;
+package org.acme.cxf.soap.utils;
 
 import io.quarkus.runtime.LaunchMode;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 
-public class BaseTest {
-    protected String getServerUrl() {
+public final class CxfServerUtils {
+    private CxfServerUtils() {
+    }
+
+    public static String getServerUrl() {
         Config config = ConfigProvider.getConfig();
         final int port = LaunchMode.current().equals(LaunchMode.TEST) ? config.getValue("quarkus.http.test-port", Integer.class)
                 : config.getValue("quarkus.http.port", Integer.class);
-        return String.format("http://localhost:%d", port);
+        return "http://localhost:%d%s".formatted(port, config.getValue("quarkus.cxf.path", String.class));
     }
 }
